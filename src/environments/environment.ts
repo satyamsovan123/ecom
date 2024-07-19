0976ab4bd500609d22348d072d70ae1938a30438
Satyam Sovan Mishra <33446554+satyamsovan123@users.noreply.github.com>
2024-07-19 10:40:34 +0530

    Fix Firebase URL strategy

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
-  backendUrl: 'http://localhost:3000/api/v1',
+  backendUrl: 'https://ecom-server-gzxu.onrender.com/api/v1',
   stripeKey:
     'pk_test_51Kv2hcSHD0alqRG2DT4J3jhmpIT293PQIszvT49j8WLB49B4GYklMqKhs6eRYqGbdGQ01ut6YkYuPthPku8vc4CH006qIavl6b',
 };
